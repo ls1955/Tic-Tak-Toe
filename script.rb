7275ad30036bdd4acb1cc1ec25016c4549fd0766
Cheong <xxlsxx1955@gmail.com>
2022-09-20 16:15:38 +0800

    Complete project

--- script.rb
+++ script.rb
@@ -1,37 +1,117 @@
-class Game
-	# probably where the main game loop located
-	# where the outcome is decided
-end
+# frozen_string_literal: true
 
-class Board
-	# render layout, store inputs from players
-	attr_accessor :layout
+# The main event of game
+class Game
   def initialize
-    @layout = Array.new(3) {Array.new(3, "")}
+    @board = Board.new
+    @player1 = Player.new
+    @player2 = Player.new('Player-2', 'X')
+    @game_over = false
+    @winner = nil
   end
 
-	def render_board
-		@layout.each_with_index do |row, i|
-			row.each_with_index do |column, j|
-				puts @layout[i][j]
-			end
-		end
-	end
+  public
+  def run_game
+	  until @game_over
+      column_index, row_index = prompt_user(@player1)
+      next unless column_index.between?(0, 2) && row_index.between?(0, 2) && @board.layout[row_index][column_index] == ' '
+      @player1.draw_point(@board, row_index, column_index)
+      @board.render_board
+      check_outcome(@board, @player1.draw_symbol, @player2.draw_symbol)
+      break if @game_over
+      column_index, row_index = prompt_user(@player2)
+      next unless column_index.between?(0, 2) && row_index.between?(0, 2) && @board.layout[row_index][column_index] == ' '
+      @player2.draw_point(@board, row_index, column_index)
+      @board.render_board
+      check_outcome(@board, @player1.draw_symbol, @player2.draw_symbol)
+      break if @game_over
+    end
+
+    end_prompt
+  end
+
+  def start_prompt
+    puts 'The game has began.'
+    puts 'Choose a point by type in a number between 0 and 2.'
+    puts "\n\n"
+  end
+
+  def end_prompt
+    puts "Thank you for playing."
+    puts "The winner is #{winner.name} || 'nobody'."
+  end
+
+  private
+  attr_accessor :game_over, :winner
+
+  def prompt_user(user)
+    puts "#{user.name} please select a row."
+    row_index = gets.to_i
+    puts "#{user.name} please select a column."
+    column_index = gets.to_i
+    [column_index, row_index]
+  end
+
+  def check_outcome(board, symbol1, symbol2)
+    board.layout.each do |row|
+      if row.all?(symbol1)
+        @game_over = true
+        @winner = @player1
+      elsif row.all?(symbol2)
+        @game_over = true
+        @winner = @player2
+      end
+    end
+
+    if board.layout[0][0] == board.layout[1][1] && board.layout[1][1] == board.layout[2][2] && board.layout[0][0] != ' '
+      if board.layout[0][0] == symbol1
+        @winner = @player1
+      else
+        @winner = @player2
+      end
+      @game_over = true
+    elsif board.layout[2][0] == board.layout[1][1] && board.layout[1][1] == board.layout[0][2] && board.layout[2][0] != ' '
+      if board.layout[2][0] == symbol1
+        @winner = @player1
+      else
+        @winner = @player2
+      end
+      @game_over = true
+    end
+
+    @game_over = true unless board.layout.flatten.include?(' ')
+  end
 end
 
+# The board where the marks are drawn
+class Board
+  def initialize
+    @layout = Array.new(3) { Array.new(3, ' ') }
+  end
+
+  def render_board
+    puts "\n"
+    @layout.each { |row| p row }
+    puts "\n"
+  end
+
+  attr_accessor :layout
+end
+
+# User who could insert point on board
 class Player
-	# provide input into the board
-	
-	def initialize(draw_symbol="X")
+  attr_reader :name, :draw_symbol
+
+  def initialize(name = 'Player-1', draw_symbol = 'O')
+    @name = name
     @draw_symbol = draw_symbol
   end
 
-	def draw_point(board, row_index, column_index)
-    board[row_index][column_index] = @draw_symbol
-	end
+  def draw_point(board, row_index, column_index)
+    board.layout[row_index][column_index] = @draw_symbol
+  end
 end
 
-board = Board.new
-player1 = Player.new
-player2 = Player.new("O")
-board.render_board
+game = Game.new
+game.start_prompt
+game.run_game
